0d2856854cce9b1ab788f69a777718e9c85e89aa
John Garry <john.g.garry@oracle.com>
2024-08-29 09:04:43 +0100

    ignore header for atomic

--- verify.c
+++ verify.c
@@ -853,10 +853,10 @@ static int verify_header(struct io_u *io_u, struct thread_data *td,
 	    !td->o.time_based) {
 		if (!td->o.verify_only) {
 			if ((hdr->numberio != io_u->numberio) && (aw == 0)) {
-				log_err("verify: bad header numberio %"PRIu16
-					", wanted %"PRIu16,
-					hdr->numberio, io_u->numberio);
-				goto err;
+			//	log_err("verify: bad header numberio %"PRIu16
+			//		", wanted %"PRIu16,
+			//		hdr->numberio, io_u->numberio);
+			//	goto err;
 			}
 		}
 	}
